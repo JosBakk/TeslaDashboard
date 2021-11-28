751c3230ee0fa8cc01c3e0600f2aeaf641d08f93
JosBakk <jostein.bakkeheim@gmail.com>
2021-11-28 20:13:50 +0100

    Ordnet spillet slik at tone spilles frem til 300m fra goal, deretter spilles dovregubben

--- Games/GettingWarmer/script.js
+++ Games/GettingWarmer/script.js
@@ -1,3 +1,53 @@
+// The browser will limit the number of concurrent audio contexts
+// So be sure to re-use them whenever you can
+const myAudioContext = new AudioContext();
+
+/**
+ * Helper function to emit a beep sound in the browser using the Web Audio API.
+ * 
+ * @param {number} duration - The duration of the beep sound in milliseconds.
+ * @param {number} frequency - The frequency of the beep sound.
+ * @param {number} volume - The volume of the beep sound.
+ * 
+ * @returns {Promise} - A promise that resolves when the beep sound is finished.
+ */
+function beep(duration, frequency, volume){
+    return new Promise((resolve, reject) => {
+        // Set default duration if not provided
+        duration = duration || 200;
+        frequency = frequency || 440;
+        volume = volume || 100;
+
+        try{
+            let oscillatorNode = myAudioContext.createOscillator();
+            let gainNode = myAudioContext.createGain();
+            oscillatorNode.connect(gainNode);
+
+            // Set the oscillator frequency in hertz
+            oscillatorNode.frequency.value = frequency;
+
+            // Set the type of oscillator
+            oscillatorNode.type= "square";
+            gainNode.connect(myAudioContext.destination);
+
+            // Set the gain to the volume
+            gainNode.gain.value = volume * 0.01;
+
+            // Start audio with the desired duration
+            oscillatorNode.start(myAudioContext.currentTime);
+            oscillatorNode.stop(myAudioContext.currentTime + duration * 0.001);
+
+            // Resolve the promise when the sound is finished
+            oscillatorNode.onended = () => {
+                resolve();
+            };
+        }catch(error){
+            reject(error);
+        }
+    });
+}
+
+
 /**
  * Create google maps Map instance.
  * @param {number} lat
@@ -55,6 +105,53 @@ const getPositionErrorMessage = code => {
   }
 }
 
+function calcDist(lat1, lon1, lat2, lon2) {
+    if ((lat1 == lat2) && (lon1 == lon2)) {
+        return 0;
+    }
+    else {
+        var radlat1 = Math.PI * lat1/180;
+        var radlat2 = Math.PI * lat2/180;
+        var theta = lon1-lon2;
+        var radtheta = Math.PI * theta/180;
+        var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+        if (dist > 1) {
+            dist = 1;
+        }
+        dist = Math.acos(dist);
+        dist = dist * 180/Math.PI;
+        dist = dist * 60 * 1.1515;
+        dist = dist * 1609.344; // convert to meters
+        return dist;
+    }
+}
+
+  var tot_score=20;
+  var distance = 0;
+  var goal_pos_lat = 59.74650;
+  var goal_pos_lon = 10.20640;
+  var beep_frq;
+  var audio = new Audio("Dovregubben.mp3");
+
+  
+/**
+ * Initialize the application.
+ * Automatically called by the google maps API once it's loaded.
+*/
+function posUpdated(lat2, lon2) {
+	tot_score = tot_score + 1;
+	distance = calcDist(goal_pos_lat, goal_pos_lon, lat2, lon2);
+	document.getElementById('score').innerHTML = "Total score:" + tot_score + " Distance=" + distance;
+	if (distance < 280) {
+		audio.currentTime=140-(distance/2); //169 length
+		audio.play();
+	} else {
+		beep_frq = -0.41*distance + 800;
+		beep(200, beep_frq, 100);
+	}
+	
+}
+
 /**
  * Initialize the application.
  * Automatically called by the google maps API once it's loaded.
@@ -65,12 +162,14 @@ function init() {
   const marker = createMarker({ map, position: initialPosition });
   const $info = document.getElementById('info');
 
+
   let watchId = trackLocation({
     onSuccess: ({ coords: { latitude: lat, longitude: lng } }) => {
       marker.setPosition({ lat, lng });
       map.panTo({ lat, lng });
       $info.textContent = `Lat: ${lat.toFixed(5)} Lng: ${lng.toFixed(5)}`;
       $info.classList.remove('error');
+	  posUpdated(lat, lng);
     },
     onError: err => {
       console.log($info);
