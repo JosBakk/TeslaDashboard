1e1833511693dfb14569512ed4d0c5843d7a3905
JosBakk <jostein.bakkeheim@gmail.com>
2023-04-06 13:40:40 +0200

    Added new folder Paaske med oppgave finne Nordstrand via dovregubbens hall. Fjernet distanse fra nettside.

---
+++ Paaske/script.js
@@ -0,0 +1,180 @@
+// The browser will limit the number of concurrent audio contexts
+// So be sure to re-use them whenever you can
+const myAudioContext = new AudioContext();
+
+/**
+ * Helper function to emit a beep sound in the browser using the Web Audio API.
+ * 
+ * @param {number} duration - The duration of the beep sound in milliseconds.
+ * @param {number} frequency - The frequency of the beep sound.
+ * @param {number} volume - The volume of the beep sound.
+ * 
+ * @returns {Promise} - A promise that resolves when the beep sound is finished.
+ */
+function beep(duration, frequency, volume){
+    return new Promise((resolve, reject) => {
+        // Set default duration if not provided
+        duration = duration || 200;
+        frequency = frequency || 440;
+        volume = volume || 100;
+
+        try{
+            let oscillatorNode = myAudioContext.createOscillator();
+            let gainNode = myAudioContext.createGain();
+            oscillatorNode.connect(gainNode);
+
+            // Set the oscillator frequency in hertz
+            oscillatorNode.frequency.value = frequency;
+
+            // Set the type of oscillator
+            oscillatorNode.type= "square";
+            gainNode.connect(myAudioContext.destination);
+
+            // Set the gain to the volume
+            gainNode.gain.value = volume * 0.01;
+
+            // Start audio with the desired duration
+            oscillatorNode.start(myAudioContext.currentTime);
+            oscillatorNode.stop(myAudioContext.currentTime + duration * 0.001);
+
+            // Resolve the promise when the sound is finished
+            oscillatorNode.onended = () => {
+                resolve();
+            };
+        }catch(error){
+            reject(error);
+        }
+    });
+}
+
+
+/**
+ * Create google maps Map instance.
+ * @param {number} lat
+ * @param {number} lng
+ * @return {Object}
+ */
+const createMap = ({ lat, lng }) => {
+  return new google.maps.Map(document.getElementById('map'), {
+    center: { lat, lng },
+    zoom: 15
+  });
+};
+
+/**
+ * Create google maps Marker instance.
+ * @param {Object} map
+ * @param {Object} position
+ * @return {Object}
+ */
+const createMarker = ({ map, position }) => {
+  return new google.maps.Marker({ map, position });
+};
+
+/**
+ * Track the user location.
+ * @param {Object} onSuccess
+ * @param {Object} [onError]
+ * @return {number}
+ */
+const trackLocation = ({ onSuccess, onError = () => { } }) => {
+  if ('geolocation' in navigator === false) {
+    return onError(new Error('Geolocation is not supported by your browser.'));
+  }
+
+  return navigator.geolocation.watchPosition(onSuccess, onError, {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  });
+};
+
+/**
+ * Get position error message from the given error code.
+ * @param {number} code
+ * @return {String}
+ */
+const getPositionErrorMessage = code => {
+  switch (code) {
+    case 1:
+      return 'Permission denied.';
+    case 2:
+      return 'Position unavailable.';
+    case 3:
+      return 'Timeout reached.';
+  }
+}
+
+function calcDist(lat1, lon1, lat2, lon2) {
+    if ((lat1 == lat2) && (lon1 == lon2)) {
+        return 0;
+    }
+    else {
+        var radlat1 = Math.PI * lat1/180;
+        var radlat2 = Math.PI * lat2/180;
+        var theta = lon1-lon2;
+        var radtheta = Math.PI * theta/180;
+        var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+        if (dist > 1) {
+            dist = 1;
+        }
+        dist = Math.acos(dist);
+        dist = dist * 180/Math.PI;
+        dist = dist * 60 * 1.1515;
+        dist = dist * 1609.344; // convert to meters
+        return dist;
+    }
+}
+
+  var tot_score=20;
+  var distance = 0;
+  var goal_pos_lat = 58.900609;
+  var goal_pos_lon = 9.5783418;
+  var beep_frq;
+  var audio = new Audio("Dovregubben.mp3");
+
+  
+/**
+ * Initialize the application.
+ * Automatically called by the google maps API once it's loaded.
+*/
+function posUpdated(lat2, lon2) {
+	tot_score = tot_score + 1;
+	distance = calcDist(goal_pos_lat, goal_pos_lon, lat2, lon2);
+	//document.getElementById('score').innerHTML = "Total score:" + tot_score + " Distance=" + distance;
+	if (distance < 280) {
+		audio.currentTime=140-(distance/2); //169 length
+		audio.play();
+	} else {
+		beep_frq = -0.41*distance + 800;
+		beep(200, beep_frq, 100);
+	}
+	
+}
+
+/**
+ * Initialize the application.
+ * Automatically called by the google maps API once it's loaded.
+*/
+function init() {
+  const initialPosition = { lat: 59.32, lng: 17.84 };
+  const map = createMap(initialPosition);
+  const marker = createMarker({ map, position: initialPosition });
+  const $info = document.getElementById('info');
+
+
+  let watchId = trackLocation({
+    onSuccess: ({ coords: { latitude: lat, longitude: lng } }) => {
+      marker.setPosition({ lat, lng });
+      map.panTo({ lat, lng });
+      $info.textContent = `Lat: ${lat.toFixed(5)} Lng: ${lng.toFixed(5)}`;
+      $info.classList.remove('error');
+	  posUpdated(lat, lng);
+    },
+    onError: err => {
+      console.log($info);
+      $info.textContent = `Error: ${err.message || getPositionErrorMessage(err.code)}`;
+      $info.classList.add('error');
+    }
+  });
+}
